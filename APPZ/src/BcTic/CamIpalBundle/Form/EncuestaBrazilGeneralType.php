549fd8846db4f4bbee8bb7bb82373dea433e07c0
Leonardo Barrientos C <lbarrientos@bctic.net>
2014-12-31 19:09:21 -0300

    VERFICICAR POR VALIDAR EN FORMS
    Former-commit-id: 3e789ea31297fbad8b28e83399c7dcab9686ca82

--- APPZ/src/BcTic/CamIpalBundle/Form/EncuestaBrazilGeneralType.php
+++ APPZ/src/BcTic/CamIpalBundle/Form/EncuestaBrazilGeneralType.php
@@ -74,7 +74,7 @@ class EncuestaBrazilGeneralType extends AbstractType
             ->add('respuesta103', new SurveyType(), array('label' => 'respuesta10.3_brazil_general'))
             ->add('respuesta111', new SurveyType(), array('label' => 'respuesta11.1_brazil_general'))
             ->add('cierre','textarea', array('label' => 'Cierre'))
-            ->add('status_cierre','choice', array('label' => 'Estado de cierre', 'choices' => array('N/A' => 'N/A: No aplica','POR_VALIDAR' => 'POR VERIFICAR: Las justificaciones de cierre no han sido verificadas aún por el administrador','ABIERTA' => 'ABIERTA: Tiene incumplimientos no correctamente justificados aún', 'CERRADA' => 'CERRADA: Tiene incumplimientos cuyo cierre ha sido aceptado por el administrador.')))
+            ->add('status_cierre','choice', array('label' => 'Estado de cierre', 'choices' => array('N/A' => 'N/A: No aplica','POR_VALIDAR' => 'POR VALIDAR: Las justificaciones de cierre no han sido verificadas aún por el administrador','ABIERTA' => 'ABIERTA: Tiene incumplimientos no correctamente justificados aún', 'CERRADA' => 'CERRADA: Tiene incumplimientos cuyo cierre ha sido aceptado por el administrador.')))
             ->add('uploadedFileCierre1','file', array('label' => 'Adjunto 1','required'  => false))
             ->add('uploadedFileCierre2','file', array('label' => 'Adjunto 2','required'  => false))
             ->add('uploadedFile1','file', array('label' => 'Adjunto 1','required'  => false))
--- APPZ/src/BcTic/CamIpalBundle/Form/EncuestaObrasCivilesType.php
+++ APPZ/src/BcTic/CamIpalBundle/Form/EncuestaObrasCivilesType.php
@@ -75,7 +75,7 @@ class EncuestaObrasCivilesType extends AbstractType
             ->add('respuesta111', new SurveyType(), array('label' => 'respuesta11.1_obras_civiles'))
             ->add('respuesta121', new SurveyNoType(), array('label' => 'respuesta12.1_obras_civiles'))
             ->add('cierre','textarea', array('label' => 'Cierre'))
-            ->add('status_cierre','choice', array('label' => 'Estado de cierre', 'choices' => array('N/A' => 'N/A: No aplica','POR_VALIDAR' => 'POR VERIFICAR: Las justificaciones de cierre no han sido verificadas aún por el administrador','ABIERTA' => 'ABIERTA: Tiene incumplimientos no correctamente justificados aún', 'CERRADA' => 'CERRADA: Tiene incumplimientos cuyo cierre ha sido aceptado por el administrador.')))            
+            ->add('status_cierre','choice', array('label' => 'Estado de cierre', 'choices' => array('N/A' => 'N/A: No aplica','POR_VALIDAR' => 'POR VALIDAR: Las justificaciones de cierre no han sido verificadas aún por el administrador','ABIERTA' => 'ABIERTA: Tiene incumplimientos no correctamente justificados aún', 'CERRADA' => 'CERRADA: Tiene incumplimientos cuyo cierre ha sido aceptado por el administrador.')))            
             ->add('uploadedFileCierre1','file', array('label' => 'Adjunto 1','required'  => false))
             ->add('uploadedFileCierre2','file', array('label' => 'Adjunto 2','required'  => false))
             ->add('uploadedFile1','file', array('label' => 'Adjunto 1','required'  => false))

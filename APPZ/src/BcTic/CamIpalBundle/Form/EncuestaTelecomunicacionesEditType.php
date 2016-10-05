63e40b588f7e859cfc2a7f4c9e0770f6038f3e8d
leonardobarrientosc <lbarrientos@bctic.net>
2016-10-04 22:02:09 -0300

    Cambia separador desde , a ; - Agrega CharlaGrabada y AutoInspección a Teleomunicaciones

--- APPZ/src/BcTic/CamIpalBundle/Form/EncuestaTelecomunicacionesEditType.php
+++ APPZ/src/BcTic/CamIpalBundle/Form/EncuestaTelecomunicacionesEditType.php
@@ -202,7 +202,7 @@ class EncuestaTelecomunicacionesEditType extends AbstractType
                     'read_only' => true,
                     'disabled' => true,
                 ))
-            ->add('autoInspeccion','hidden')
+            ->add('autoInspeccion','choice', array('label' => 'Auto Inspección', 'choices' => array('N/A' => 'N/A: No aplica','A1' => 'A1','A2' => 'A2','A3' => 'A3','A4' => 'A4')))
             ->add('charlaOperativa','hidden')
             ->add('charlaGrabada','choice', array('label' => 'Charla grabada', 'choices' => array('N/A' => 'N/A: No aplica','SI' => 'Sí', 'NO' => 'No' )))
 
